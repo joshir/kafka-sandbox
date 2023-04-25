d0d773c8543016dd202ea45aa24efc76f62c174f
joshir <hareepjoshi@gmail.com>
2023-04-25 11:07:30 -0500

    initial commit

---
+++ src/main/java/com/joshir/kadministrator/KadministratorApplication.java
@@ -0,0 +1,13 @@
+package com.joshir.kadministrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadministratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadministratorApplication.class, args);
+	}
+
+}
